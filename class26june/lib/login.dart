a5bb99843e163bb7f7ba8fbfa437a56ee5eddf31
AbdulKarim <abdull007karim@gmail.com>
2022-06-28 23:34:56 +0500

    :)

--- class26june/lib/login.dart
+++ class26june/lib/login.dart
@@ -1,39 +1,82 @@
+import 'package:class26june/home.dart';
 import 'package:class26june/signup.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          child: TextField(
-            decoration: InputDecoration(
-                hintText: 'Email',
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20))),
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width*.8,
+            child: TextField(controller: emailController,
+              decoration: InputDecoration(
+                  hintText: 'Email',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10))),
+            ),
           ),
-        ),
-        SizedBox(
-            child: TextField(
-          decoration: InputDecoration(
-              hintText: 'Password',
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
-        )),
-        ElevatedButton(onPressed: () {}, child: Text('Login')),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SignUp()),
-              );
-            },
-            child: Text('Sign Up')),
-      ],
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 8),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width*.8,
+                child: TextField(controller: passwordController,
+              decoration: InputDecoration(
+                  hintText: 'Password',
+                  border:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+            )),
+          ),
+          ElevatedButton(
+              onPressed: () async {
+                try {
+                  UserCredential userCredential = await FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                          email: emailController.text.toString(),
+                          password: passwordController.text.toString());
+                          Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Home()),
+                );
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'user-not-found') {
+                    print('No user found for that email.');
+                    return showDialog(
+                    context: context,
+                    builder: (ctxt) =>  AlertDialog(
+                      title: Text("No User Found"),
+                    )
+                );
+                  } else if (e.code == 'wrong-passwdord') {
+                    print('Wrong password provided for that user.');
+                    return showDialog(
+                    context: context,
+                    builder: (ctxt) =>  AlertDialog(
+                      title: Text("Wrong Password"),
+                    )
+                );
+                  }
+                }
+              },
+              child: Text('Login')),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignUp()),
+                );
+              },
+              child: Text('Sign Up')),
+        ],
+      ),
     );
   }
 }
--- class26june/lib/signup.dart
+++ class26june/lib/signup.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -9,30 +10,50 @@ class SignUp extends StatefulWidget {
 
 class _SignUpState extends State<SignUp> {
   TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController =  TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(
-            child: TextField(controller: emailController,
+          SizedBox(width: MediaQuery.of(context).size.width*.5,
+            child: TextField(
+
+              controller: emailController,
               decoration: InputDecoration(
                   hintText: 'Email',
                   border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20))),
+                      borderRadius: BorderRadius.circular(10))),
             ),
           ),
-          SizedBox(
-            child: TextField(controller: passwordController,
+          SizedBox(width: MediaQuery.of(context).size.width*.5,
+            child: TextField(
+              controller: passwordController,
               decoration: InputDecoration(
                   hintText: 'Password',
                   border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20))),
+                      borderRadius: BorderRadius.circular(10))),
             ),
           ),
-          ElevatedButton(onPressed: () {}, child: Text('Sign Up')),
+          ElevatedButton(
+              onPressed: () async {
+                try {
+                  UserCredential userCredential = await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                          email: emailController.text.toString(),
+                          password: passwordController.text.toString());
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    print('The password provided is too weak.');
+                  } else if (e.code == 'email-already-in-use') {
+                    print('The account already exists for that email.');
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              },
+              child: Text('Sign Up')),
         ],
       ),
     );
